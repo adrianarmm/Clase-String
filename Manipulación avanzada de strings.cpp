f7bae379b8ef2887aff1df1d0d4b4ba4062cdb8d
adrianarmm <areyemor@myuax.com>
2023-11-23 12:44:16 +0100

    6

--- Manipulación avanzada de strings.cpp
+++ Manipulación avanzada de strings.cpp
@@ -1,3 +1,16 @@
-//
-// Created by Adriana Reyes Morera on 23/11/23.
-//
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string str = "C++ es un lenguaje de programación.";
+
+    std::string sub_str = str.substr(13, 9);
+
+
+    std::string new_str = "La " + str;
+
+    std::cout << "Subcadena: " << sub_str << std::endl;
+    std::cout << "Cadena resultante: " << new_str << std::endl;
+
+    return 0;
+}
