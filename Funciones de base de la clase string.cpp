bf280351b51b8c8fe392b45ed19e2b989eeb6dc9
adrianarmm <areyemor@myuax.com>
2023-11-23 12:19:16 +0100

    2

---
+++ Funciones de base de la clase string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string sentence = "OpenAI is a leading AI research lab.";
+    std::cout << "Cadena: " << sentence << '\n';
+    std::cout << "Tamaño de la cadena: " << sentence.size() << '\n';
+    std::cout << "Primer caracter: " << sentence[0] << '\n';
+    std::cout << "Último caracter: " << sentence[sentence.size() - 1] << '\n';
+    std::cout << "¿Está la cadena vacía? " << (sentence.empty() ? "Sí" : "No") << '\n';
+
+    return 0;
+}
