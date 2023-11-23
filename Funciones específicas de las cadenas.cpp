5bfbb03834b87fe7d3b1a648d7495d434f9373e0
adrianarmm <areyemor@myuax.com>
2023-11-23 12:42:23 +0100

    5

--- Funciones específicas de las cadenas.cpp
+++ Funciones específicas de las cadenas.cpp
@@ -1,3 +1,22 @@
-//
-// Created by Adriana Reyes Morera on 23/11/23.
-//
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string sentence = "El lenguaje C++ es poderoso.";
+    std::string sub_str = "C++";
+
+    size_t pos = sentence.find(sub_str);
+
+    if (pos != std::string::npos) {
+        std::cout << "Subcadena encontrada en la posición: " << pos << std::endl;
+
+
+        std::string new_sentence = sentence;
+        new_sentence.replace(pos, sub_str.length(), "Python");
+        std::cout << "Cadena resultante: " << new_sentence << std::endl;
+    } else {
+        std::cout << "Subcadena no encontrada." << std::endl;
+    }
+
+    return 0;
+}
